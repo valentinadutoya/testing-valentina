126dae7ce5cf397b381fe91144824fbcbc8ffe2b
valentinadutoya <laravalentinadutoya2019@gmail.com>
2024-08-14 11:43:13 -0300

    agregu el test de modificar el email

---
+++ examples/tutorial/tests/test_user_mod.py
@@ -0,0 +1,22 @@
+import pytest
+
+from flaskr.db import get_db
+
+
+
+def test_update(client, auth, app):
+    auth.login()
+    assert client.get("/1/update").status_code == 200
+    client.post("/1/update", data={"title": "updated", "contraseña": ""})
+
+    with app.app_context():
+        db = get_db()
+        post = db.execute("SELECT * FROM post WHERE id = 1").fetchone()
+        assert post["title"] == "updated"
+
+
+@pytest.mark.parametrize("path", ("/create", "/1/update"))
+def test_create_update_validate(client, auth, path):
+    auth.login()
+    response = client.post(path, data={"title": "", "body": ""})
+    assert b"Title is required." in response.data
